237c1cb192e4896bcd2e478251009062ffe0d1cd
Richard Iyama <richardiyama@yahoo.com>
2020-06-27 15:43:14 +0100

    Admin implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,1 @@
 require('./bootstrap');
-window.Vue = require('vue');
-
-import App from './App';
-import { router } from './router/router.js'
-import { store } from './vuex'
-
-const app = new Vue({
-    el: '#app',
-    render: h => h(App),
-    router: router,
-    store
-});
--- resources/js/components/Footer.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <footer class="main-footer"><strong>Copyright © 2020 <a href="#" target="_blank">TeacherTurf</a>.</strong> All rights reserved.
-          </footer>
-</template>
